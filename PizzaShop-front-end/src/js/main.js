5cbb2f2e49cffc97afb99a8f1fcfc8d5b1ca768d
Tim <tim2kaspar@gmail.com>
2020-11-13 12:30:40 +0100

    == -> === for status check in request

--- PizzaShop-front-end/src/js/main.js
+++ PizzaShop-front-end/src/js/main.js
@@ -99,7 +99,7 @@ xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
 
 
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState === 4 && this.status === 200) {
         let pizzas = JSON.parse(xhttp.responseText);
         showPizzas(pizzas);
         //show Json in console
